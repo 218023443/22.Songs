d1440e104e1c9f3d6054353a6f5b2ca6bd092d96
A252557 <A252557@LSBSA-3WCCHW3>
2023-12-07 02:09:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs holiday = new Songs("Holiday", "Green day",5);
+            Songs kashmir = new Songs("kashmir", "Green day", 3);
+            Songs kashmir2 = new Songs("kashmir", "Green day", 3);
+
+            Console.WriteLine("You have "+Songs.songCount+" list of songs.");
+            Console.ReadLine(); 
+
+        }
+    }
+}
